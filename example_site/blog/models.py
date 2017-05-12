9e0b9bbb75539018e73490b1ba7b240d610c4048
Ravi RT Merugu <rrmerugu@gmail.com>
2017-05-12 20:22:16 +0530

    model methods updated

--- example_site/blog/models.py
+++ example_site/blog/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -11,5 +12,24 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, db_index=True)
     text = models.TextField()
+    is_draft = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, blank=False)
+    
+    def make_draft(self):
+        self.is_draft = True
+        self.save()
+
+    def make_published(self):
+        self.is_draft = False
+        self.save()
+        
+    def generated_slug(self):
+        self.slug = slugify(self.title)
+        
+    def save(self, *args, **kwargs):
+        if not self.slug: # generate slug if not exist
+            self.generated_slug()
+        super(BlogPost, self).save(*args, **kwargs)
+
+        
